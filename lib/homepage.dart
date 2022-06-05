59fdf6f7dd25117cf5977a3e7a3079ea384d688c
Isha41 <076bct020.isha@sagarmatha.edu.np>
2022-06-05 08:06:55 +0545

    background and appbar color red

---
+++ lib/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  const homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          'Dice',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
